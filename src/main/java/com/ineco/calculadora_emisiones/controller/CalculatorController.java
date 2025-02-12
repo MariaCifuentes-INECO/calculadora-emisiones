6b5638cf1f5392a5e0f6330c94de00c9af7b490b
mariaCifuentes-INECO <maria.cifuentes@ineco.com>
2025-02-12 19:26:17 +0100

    endPoint gráfico GEI

---
+++ src/main/java/com/ineco/calculadora_emisiones/controller/CalculatorController.java
@@ -0,0 +1,48 @@
+package com.ineco.calculadora_emisiones.controller;
+
+import com.ineco.calculadora_emisiones.model.request.CreateGenericCaseRequest;
+import com.ineco.calculadora_emisiones.service.CalculosEmisionesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "Calculator Controller", description = "Microservicio encargado de exponer operaciones CRUD sobre los datos de la calculadora.")
+public class CalculatorController {
+
+    private final CalculosEmisionesService calculosEmisionesService;
+
+    /**
+     * Obtiene los datos necesarios para representar el gráfico GEI sistema.
+     *
+     * @param request Datos de entrada para el cálculo.
+     * @return Un mapa con las listas de datos necesarios para el gráfico.
+     */
+    @PostMapping("/graficoGEISistema")
+    @Operation(
+            operationId = "Obtener los datos necesarios para représentar el gráfico GEI sistema",
+            description = "Operación de lectura",
+            summary = "Se devuelve un objeto con todas las listas de datos necesarios para el gráfico GEI sistema.")
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Map.class)))
+    public ResponseEntity<Map<String, List<Double>>> getGraficoGEISistema(
+            @RequestBody CreateGenericCaseRequest request) {
+
+        Map<String, List<Double>> datos = calculosEmisionesService.obtenerGraficoGEI(request);
+
+        return ResponseEntity.ok(datos);
+    }
+
+}
--- src/main/java/com/ineco/calculadora_emisiones/service/CalculosEmisionesService.java
+++ src/main/java/com/ineco/calculadora_emisiones/service/CalculosEmisionesService.java
@@ -1,4 +1,11 @@
 package com.ineco.calculadora_emisiones.service;
 
+import com.ineco.calculadora_emisiones.model.request.CreateGenericCaseRequest;
+
+import java.util.List;
+import java.util.Map;
+
 public interface CalculosEmisionesService {
+
+    Map<String, List<Double>> obtenerGraficoGEI(CreateGenericCaseRequest request);
 }
